8422016bd1e465b35c4629d33437246fc60c7f58
JYS <jeces@naver.com>
2022-12-23 12:49:02 +0900

    12_23

--- app/src/main/java/com/example/applicationjeces/chat/ChatData.kt
+++ app/src/main/java/com/example/applicationjeces/chat/ChatData.kt
@@ -2,7 +2,7 @@ package com.example.applicationjeces.chat
 
 import com.google.firebase.Timestamp
 import java.util.*
-
+/* */
 data class ChatData(
     val chatroomidx: String,
     val content: String,
