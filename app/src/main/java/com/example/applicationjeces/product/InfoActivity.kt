28274dd345bbfa6de1f80a3fbe68b2f727e86dff
jeces <jeces@naver.com>
2023-08-12 17:21:09 +0900

    08_12

--- app/src/main/java/com/example/applicationjeces/product/InfoActivity.kt
+++ app/src/main/java/com/example/applicationjeces/product/InfoActivity.kt
@@ -21,26 +21,51 @@ import kotlinx.android.synthetic.main.fragment_info.*
 
 class InfoActivity : AppCompatActivity() {
 
-    class InfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_info)
+        val pId = intent.getStringExtra("ID").toString()
+        val pName = intent.getStringExtra("productName").toString()
+        val productPrice = intent.getStringExtra("productPrice")
+        val productDescription = intent.getStringExtra("productDescription")
+        val productCount = intent.getStringExtra("productCount")
+        val pChatCount = intent.getStringExtra("pChatCount")
+        val pViewCount = intent.getStringExtra("pViewCount")
+        val pHeartCount = intent.getStringExtra("pHeartCount")
+        val productBidPrice = intent.getStringExtra("productBidPrice")
 
-            val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
-            bottomNavigation.setOnNavigationItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.nav_info -> {
-                        // InfoFragment 표시
-                        val fragment = InfoFragment()
-                        val transaction = supportFragmentManager.beginTransaction()
-                        transaction.replace(R.id.fragmentContainer, fragment)
-                        transaction.commit()
-                        true
-                    }
-                    // 기타 메뉴 항목 처리
-                    else -> false
+        // 기본적으로 InfoFragment를 표시합니다.
+        val fragment = InfoFragment().apply {
+            arguments = Bundle().apply {
+                putString("ID", pId)
+                putString("productName", pName)
+                putString("productPrice", productPrice)
+                putString("productDescription", productDescription)
+                putString("productCount", productCount)
+                putString("pChatCount", pChatCount)
+                putString("pViewCount", pViewCount)
+                putString("pHeartCount", pHeartCount)
+                putString("productBidPrice", productBidPrice)
+            }
+        }
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_info -> {
+                    // InfoFragment 표시
+                    val fragment = InfoFragment()
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fragmentContainer, fragment)
+                    transaction.commit()
+                    true
                 }
+                // 기타 메뉴 항목 처리
+                else -> false
             }
         }
     }
